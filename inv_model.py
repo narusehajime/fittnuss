5d1564b670f5e0beacac4afeff9872e47580d3ae
rimalim2009 <42640161+rimalim2009@users.noreply.github.com>
2018-10-01 14:21:52 +0900

    Add files via upload

--- inv_model.py
+++ inv_model.py
@@ -3,9 +3,7 @@ from scipy import interpolate as ip
 import forward_model as fmodel
 import time as tm
 import scipy.optimize as opt
-#import matplotlib.pyplot as plt
-from configparser import ConfigParser as scp
-#from matplotlib.font_manager import FontProperties
+from configparser import ConfigParser 
 
 Nfeval = 1 #number of epochs in optimization calculation
 deposit_o = [] #Observed thickness of a tsunami deposit
@@ -13,10 +11,7 @@ spoints = [] #Location of sampling points
 observation_x_file=[]
 observation_deposit_file=[]
 resultfile=[]
-inversion_x_file=[]
 obj_func_file=[]
-inversion_startvalues_file=[]
-inversion_deposit_file=[]
 bound_values = []
 start_params = []
 
@@ -25,26 +20,21 @@ def read_setfile(configfile):
     """
     read setting file (config.ini) and set parameters to the inverse model
     """
-    global observation_x_file, observation_deposit_file, inversion_result_file,\
-        inversion_x_file, inversion_deposit_file, inversion_ofunction_file, \
-        inversion_startvalues_file, start_params, bound_values
+    global observation_x_file, observation_deposit_file, resultfile, obj_func_file, start_params, bound_values
 
-    parser = scp()
+    parser = ConfigParser()
     parser.read(configfile)#read a setting file
 
     #set file names
     observation_x_file = parser.get("Import File Names", "observation_x_file")
     observation_deposit_file = parser.get("Import File Names",\
                                           "observation_deposit_file")
-    inversion_result_file = parser.get("Export File Names",\
-                                       "inversion_result_file")
-    inversion_x_file = parser.get("Export File Names", "inversion_x_file")
-    inversion_deposit_file = parser.get("Export File Names",\
-                                        "inversion_deposit_file")
-    inversion_ofunction_file=parser.get("Export File Names",\
-                                        "inversion_ofunction_file")
-    inversion_startvalues_file=parser.get("Export File Names",\
-                                          "inversion_startvalues_file")
+    resultfile = parser.get("Export File Names",\
+                                        "resultfile")
+    
+    obj_func_file= parser.get("Export File Names",\
+                                        "obj_func_file")
+    
 
     #Read starting values
     Rw0_text = parser.get("Inversion Options", "Rw0")
@@ -144,35 +134,25 @@ def readdata(spointfile, depositfile):
     
     return (spoints, deposit_o)
 
-def save_result(resultfile, spointfile, depositfile, res):
-    """
-    Save the inversion results
-    """
-    #Calculate the forward model using the inversion result
-    (x, C, x_dep, deposit_c) = fmodel.forward(res.x)
-
-    #Save the best result
-    fmodel.export_result(spointfile, depositfile)
-    np.savetxt(resultfile, res.x)
 
 def save_multiple_results(resultfile, obj_func_file, results):
     """
     Save the inversion results
     """
     #Calculate the forward model using the inversion result
+    
     for i in range(len(results)):
         #Save the best result
 #        np.savetxt(resultfile, results[i].x, 'ab')
 #        np.savetxt(obj_func_file, results[i].fun, 'ab')
-        resultfile= open('result.txt', 'a')
-        np.savetxt(resultfile, results[i].x)
-        obj_func_file= open('objfunc.txt', 'a')
-        np.savetxt(obj_func_file, results[i].fun)
+        resultfile= open('result.csv', 'a')
+        np.savetxt(resultfile, np.array(results[i].x))
+        obj_func_file= open('objfunc.csv', 'a')
+        np.savetxt(obj_func_file, np.array(results[i].fun))
         resultfile.close()
         obj_func_file.close()
-
-    
-
+       
+           
 def callbackF(x):
     """
     A function to display progress of optimization
@@ -181,65 +161,6 @@ def callbackF(x):
     print('{0: 3d}  {1: 3.0f}   {2: 3.2f}   {3: 3.2f}   {4: 3.3f}    {5: 3.6f}'.format(Nfeval, x[0], x[1], x[2], x[3], costfunction(x)))
     Nfeval +=1
 
-#def plot_result(res):
-    """
-    Plot result of inversion
-    """
-    
-#    deposit_c = []
-#    totalthick = []
-#    symbollist = ['-','-.','--',':','-','-.']
-#    
-#    #Calculate results from given parameter sets
-#    for j in range(len(params)):
-#        deposit_c.append([])
-#        totalthick.append([])
-#        (x, C, x_dep, deposit_c[j]) = fmodel.forward(params[j]) #best result
-#        cnum = fmodel.cnum #number of grain size classes
-#        totalthick[j] = np.sum(deposit_c[j],axis=0)
-    
-    #Correction of offset of x coordinate
-    #This is necessary because the seaward end of calculation domain is
-    #assumed to be x=0
-#    x_dep = x_dep + spoints[0]
-    
-    
-    #Formatting plot area
-#    plt.figure(num=None, figsize=(7, 8.5), dpi=150, facecolor='w', edgecolor='k')
-#    fp = FontProperties(size=9)
-#    plt.rcParams["font.size"] = 9
-#        
-#    plt.subplot(cnum+1,1,1)
-#    plt.plot(spoints, np.sum(deposit_o,axis=0),marker='o', markersize=4,\
-#             fillstyle='none', linestyle = 'None', label = "Observation")
-#    for l in range(len(params)):
-#        plt.plot(x_dep[xmin:xmax], totalthick[l][xmin:xmax], symbollist[l],\
-#                 linewidth = 0.75, label = labels[l])
-#    plt.title('Total Thickness')
-#    plt.ylabel('Thickness (m)')
-#    plt.yscale("log")
-#    plt.ylim(0,0.4)
-#    plt.legend(prop = fp, loc='best', borderaxespad=1)
-#    
-#    for i in range(cnum):
-#        plt.subplot(cnum+1,1,i+2) #Prepare graphs
-#        plt.plot(spoints, deposit_o[i,:], marker = 'o', markersize=4,\
-#                 fillstyle='none', color = 'k', linestyle = 'None',\
-#                 label = "Observation")
-#        for k in range(len(params)):
-#            plt.plot(x_dep[xmin:xmax], deposit_c[k][i,xmin:xmax],\
-#                     symbollist[k], linewidth = 0.75, label = labels[k])
-#        if i == cnum - 1:
-#            plt.xlabel('Distance (m)')
-#        plt.ylabel('Sed. Vol. per \n Unit Area (m)')
-#        d = fmodel.Ds[i]*10**6
-#        gclassname='{0:.0f} $\mu$m'.format(d[0])
-#        plt.yscale("log")
-#        plt.ylim(0,0.4)
-#        plt.title(gclassname)
-#    
-#    plt.subplots_adjust(hspace=0.7)
-#    plt.show()
 
 def inv_multistart():
     """
@@ -248,54 +169,26 @@ def inv_multistart():
 
         
     #Read a configuration file
-    read_setfile('config_sendai.ini') 
-
-    #Read the measurement data
-    (spoints, deposit_o) = readdata(observation_x_file,\
-                                    observation_deposit_file) 
-    """
-    #list of starting values
-    initU = [2, 4, 6]
-    initH = [3, 5, 8]
-    initC = [[0.01, 0.01, 0.01]]
-    
-    #Perform the multistart optimization
-    #res = []
-    #initparams = []
-    #for i in range(len(initU)):
-        for j in range(len(initH)):
-            for k in range(len(initC)):
-                init = [start_params[0],initU[i],initH[j]]
-                init.extend(initC[k])
-                initparams.append(init)
-    
-
-    
- """
-    #For future implementation of parallelization
-    #pool = Pool()
-    #res = pool.map(optimization, initparams)
+#    read_setfile('config_sendai.ini')
+#
+#    #Read the measurement data
+#    (spoints, deposit_o) = readdata(observation_x_file,\
+#                                    observation_deposit_file) 
+#    
     result = list(map(optimization, start_params))
-#    res_fun = result[0].fun
-#    print("result:",result)
-#    print("length:",len(result))
-#    res_x = map_obj.x
-    
-#    result = list(map(optimization, start_params))
-#    return res_x, start_params,res_fun
     return result, start_params
 
 if __name__=="__main__":
-#    #Set initial conditions of inverse model
-    read_setfile('config_sendai.ini') 
+#    Set initial conditions of inverse model
+    read_setfile('config_sendai.ini')
 
     #Read measurement data set
     (spoints, deposit_o) = readdata(observation_x_file, observation_deposit_file)
 
     #Conduct optimization
-    result,start_params = inv_multistart()
+    res,start_params = inv_multistart()
 
-    save_multiple_results('result.txt', 'objfun.txt', result)
+    save_multiple_results(resultfile, obj_func_file, res)
 
     #xlist = np.loadtxt('inversion_result_sendai03.txt', delimiter=',')
     #params = [xlist[0],xlist[4],xlist[5],xlist[6],xlist[7],xlist[8]]
